9575037d1b7a15f1417907759b1ba3bc0f469e9d
Ajay Singh <helloajaysingh1@gmail.com>
2022-01-30 23:10:55 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,6 @@ import "./App.css";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 class App extends Component {
-  // state = {
-  //   age: 21,
-  // };
-
-  // handleIncrement = () => {
-  //   this.setState({
-  //     ...this.state,
-  //     age: this.state.age + 1,
-  //   });
-  // };
-
-  // handleDecrement = () => {
-  //   this.setState({
-  //     ...this.state,
-  //     age: this.state.age - 1,
-  //   });
-  // };
-
   render() {
     return (
       <div className="App">
@@ -28,6 +10,32 @@ class App extends Component {
         <button onClick={this.props.onAgeUp}>increment</button>
         <button onClick={this.props.onAgeDown}>descrement</button>
         <button onClick={this.props.onAgeReset}>reset</button>
+        <div className="list">
+          <ul>
+            {this.props.history.map((item) => (
+              <li key={item.id} className="listItem">
+                {item.age}
+                <button
+                  className="delete"
+                  onClick={() => {
+                    this.props.onDeleteItem(item.id);
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 0 24 24"
+                    width="24px"
+                    fill="#000000"
+                  >
+                    <path d="M0 0h24v24H0V0z" fill="none" />
+                    <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
+                  </svg>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
@@ -36,14 +44,16 @@ class App extends Component {
 const mapStateToProps = (state) => {
   return {
     age: state.age,
+    history: state.history,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onAgeUp: () => dispatch({ type: "AGE_UP" }),
-    onAgeDown: () => dispatch({ type: "AGE_DOWN" }),
+    onAgeUp: () => dispatch({ type: "AGE_UP", value: 1 }),
+    onAgeDown: () => dispatch({ type: "AGE_DOWN", value: 1 }),
     onAgeReset: () => dispatch({ type: "AGE_RESET" }),
+    onDeleteItem: (itemId) => dispatch({ type: "DELETE_ITEM", key: itemId }),
   };
 };
 
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,6 @@
 const initalState = {
   age: 21,
+  history: [],
 };
 
 const reducer = (state = initalState, action) => {
@@ -7,15 +8,27 @@ const reducer = (state = initalState, action) => {
     case "AGE_UP":
       return {
         ...state,
-        age: state.age + 1,
+        age: state.age + action.value,
+        history: state.history.concat({ id: Math.random(), age: state.age }),
       };
     case "AGE_DOWN":
       return {
         ...state,
-        age: state.age - 1,
+        age: state.age - action.value,
+        history: state.history.concat({ id: Math.random(), age: state.age }),
+      };
+    case "DELETE_ITEM":
+      // const item = state.history.find((x) => x.id === action.key);
+      // console.log(item);
+      return {
+        ...state,
+        history: state.history.filter((element) => element.id !== action.key),
       };
     case "AGE_RESET":
-      return { age: initalState.age };
+      return {
+        age: initalState.age,
+        history: [],
+      };
     default:
       return state;
   }
